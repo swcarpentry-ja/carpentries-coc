6b90d1370ee2d682349e6df0c9036198f805240d
maneesha sane <maneeshasane@gmail.com>
2019-05-29 09:21:04 -0400

    better 404 message

--- conf.py
+++ conf.py
@@ -46,14 +46,14 @@ source_suffix = ['.rst', '.md']
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx_markdown_tables',
-              # 'notfound.extension',
+              'notfound.extension',
               ]
 
-# notfound_context = {
-#         'body': '<h1>Oops, something is not right here, you know.</h1>',
-# }
+notfound_context = {
+        'body': '<h1>This page may have moved.</h1> <p>Please select a page from the side menu or contact team@carpentries.org if you need additional help.</p>',
+}
 
-# notfound_no_urls_prefix = True
+notfound_no_urls_prefix = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
